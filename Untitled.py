53b67ecb37218ee0ea6d19b2db774f4eca7b3e66
Arsen-bjj <78781689+Arsen-bjj@users.noreply.github.com>
2021-02-16 13:35:53 +0600

    практика 3-4

---
+++ Untitled.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+if 1:
+ print("hello 1") 
+
+
+# In[9]:
+
+
+a = 3
+if a == 3:
+    print("Здесь а=",а) 
+
+
+# In[10]:
+
+
+a = 3
+if a == 3:
+    print("Здесь а=",a)
+
+
+# In[12]:
+
+
+if a> 1:
+    print("hello 3")
+
+
+# In[18]:
+
+
+lst=[1,2,3]
+if lst:
+    print("hello 4")
+
+
+# In[19]:
+
+
+a=3
+if a>2:
+    print("H")
+else:
+    print("L")
+
+
+# In[20]:
+
+
+a=1
+if a>2:
+    print("H")
+else:
+    print("L")
+
+
+# In[22]:
+
+
+a=17
+b=True if a>10 else False
+print(b)
+
+
+# In[23]:
+
+
+a=int(input("введите число:"))
+if a<0:
+    print("Neg")
+elif a==0:
+    print("Zero")
+else:
+    print("Pos")
+
+
+# In[24]:
+
+
+a=0
+while a<7:
+    print("A")
+    a+=1
+
+
+# In[ ]:
+
+
+a=0
+while a==0:
+    print("A")
+
+
+# In[ ]:
+
+
+a=0
+while a>=o:
+    if a==7:
+        break
+        a+=1
+    print("A")
+
+
+# In[ ]:
+
+
+
+
